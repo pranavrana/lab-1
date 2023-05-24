9f1d839c8f973d53e76be31aac3ae98c69eef6b3
pranavrana <pranavrana1258@gmail.com>
2023-05-24 18:35:19 -0400

    First commit

---
+++ basic.py
@@ -0,0 +1,17 @@
+# Variable initialization
+a = 10
+b = 50
+
+# Conditional Statement
+if a > b:
+    # Print output
+    print("a is greater than b")
+else:
+    print("b is greater than a")
+
+# Function Declaration
+def greeting(username):
+    print(f"Hello {username}")
+
+# Calling a function
+greeting('Pranav')
